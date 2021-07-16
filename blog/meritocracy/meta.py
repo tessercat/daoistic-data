060fc40860aa997f960892bcb4152d085dbe5939
Peter Christensen <peter@daoistic.ca>
2021-07-15 21:54:23 -0600

    Comes to naught.

--- blog/meritocracy/meta.py
+++ blog/meritocracy/meta.py
@@ -1,5 +1,5 @@
 {
-    'lede': 'Lüshi Chunqiu on why micro-managers fail and what true meritocracy looks like. 審分覽 知度 3.',
+    'lede': 'Lüshi Chunqiu on trusting people to do the right thing. 審分覽 知度 3.',
     'archive': 'various',
     'published': True
 }
